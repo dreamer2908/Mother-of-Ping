38ae7dd800e350d1a81bbe6289e754c9e4f51135
dreamer2908 <dreamer2908@gmail.com>
2019-08-08 14:47:58 +0700

    Code base as of 2019-08-08 Add some misc func, input txt & csv parsers Fix ping() exception with invalid hostname, timeout, and unreachable Add a test backgroundPing()

--- Mother-of-Ping_CLI/Program.cs
+++ Mother-of-Ping_CLI/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace Mother_of_Ping_CLI
 {
@@ -9,18 +10,79 @@ namespace Mother_of_Ping_CLI
     {
         static void Main(string[] args)
         {
-            string ipAddr = "10.30.11.110";
-            int timeout = 5000;
-            int bufferSize = 32;
-            int ttl = 128;
-            bool wait1s = true;
+            //string ipAddr = "10.30.11.110";
+            //int timeout = 5000;
+            //int bufferSize = 32;
+            //int ttl = 128;
+            //bool wait1s = true;
 
-            for (int i = 0; i < 100; i++)
+            //for (int i = 0; i < 0; i++)
+            //{
+            //    int result = tools.ping(ipAddr, timeout, bufferSize, ttl, out string replyAddr, out long replyTime, out int replyTtl, wait1s, 1000);
+            //    Console.WriteLine(tools.pingStatusTable[result] + " " + replyAddr + " " + replyTime.ToString() + " " + replyTtl.ToString());
+            //}
+
+            List<string[]> hostList = tools.csvHostListParser(@"D:\Code\MonitorHost\IP_List_2019-07-05 - test 1.csv", false);
+            tools.printListOfStringArray(hostList);
+
+            int numOfHost = hostList.Count;
+            bool[] threadAliveSignal = new bool[numOfHost];
+            bool[] fllushLogSignal = new bool[numOfHost];
+            string[] actualHostList = new string[numOfHost];
+            bool continueMonitor = true;
+
+            Console.WriteLine("numOfHost: " + numOfHost.ToString());
+
+            for (int i = 0; i < numOfHost; i++)
             {
-                int result = tools.ping(ipAddr, timeout, bufferSize, ttl, wait1s, out string replyAddr, out long replyTime, out int replyTtl);
-                Console.WriteLine(tools.pingStatusTable[result] + " " + replyAddr + " " + replyTime.ToString() + " " + replyTtl.ToString());
+                int threadId = i;
+                string hostname = hostList[i][0];
+                int period = 1000;
+                int timeout = 1000;
+                int bufferSize = 32;
+                int ttl = 128;
+                var thread = new Thread(() => tools.backgroundPing(hostname, period, timeout, bufferSize, ttl, ref threadAliveSignal[threadId], ref fllushLogSignal[threadId], ref continueMonitor, ref actualHostList[threadId]));
+                thread.Start();
             }
 
+            for (int i = 0; i < 30; i++)
+            {
+                Console.WriteLine("threadAliveSignal: ");
+                //tools.printBoolArray(threadAliveSignal);
+                Console.WriteLine("True: " + tools.countValueBoolArray(threadAliveSignal, true).ToString());
+                Console.WriteLine("False: " + tools.countValueBoolArray(threadAliveSignal, false).ToString());
+
+                for (int j = 0; j < numOfHost; j++)
+                {
+                    if (threadAliveSignal[j] == false)
+                    {
+                        Console.WriteLine("Missed #" + j.ToString() + " of " + actualHostList[j]);
+                    }
+                }
+
+                tools.sanitizeBoolArray(threadAliveSignal);
+
+                Thread.Sleep(1000);
+            }
+
+            continueMonitor = false;
+
+            // check if the target threads received is the same as what main sended
+            //for (int i = 0; i < numOfHost; i++)
+            //{
+            //    string target = hostList[i][0];
+            //    string realTarget = actualHostList[i];
+            //    if (target == realTarget)
+            //    {
+            //        Console.WriteLine("Host " + i.ToString() + " matches");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Host " + i.ToString() + " NOT matches");
+            //    }
+            //}
+
+            Console.ReadLine();
         }
     }
 }
--- Mother-of-Ping_CLI/tools.cs
+++ Mother-of-Ping_CLI/tools.cs
@@ -1,13 +1,148 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Net;
 using System.Net.NetworkInformation;
 using System.Threading;
+using Microsoft.VisualBasic.FileIO;
+using System.Diagnostics;
 
 namespace Mother_of_Ping_CLI
 {
     class tools
     {
+        #region misc
+        private static void sanitizeStringArray(string[] elements)
+        {
+            for (int i = 0; i < elements.Length; i++)
+            {
+                if (elements[i] == null)
+                    elements[i] = string.Empty;
+
+                elements[i] = elements[i].Trim();
+            }
+        }
+
+        private static string[] sanitizeStringArray(string[] fields, int numberOfElements)
+        {
+            // make sure it contains exactly <numberOfElements> elements
+            string[] elements = new string[numberOfElements];
+
+            for (int i = 0; i < fields.Length && i < numberOfElements; i++)
+                elements[i] = fields[i];
+
+            // and they're not null
+            sanitizeStringArray(elements);
+
+            return elements;
+        }
+
+        public static void sanitizeBoolArray(bool[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+                arr[i] = false;
+        }
+
+        public static int countValueBoolArray(bool[] arr, bool value)
+        {
+            int count = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == value)
+                    count++;
+            }
+
+            return count;
+        }
+        #endregion
+
+        #region printer
+        public static void printListOfStringArray(List<string[]> l)
+        {
+            for (int i = 0; i < l.Count; i++)
+            {
+                string[] arr = l[i];
+                Console.WriteLine(i.ToString() + ":");
+                foreach (string s in arr)
+                {
+                    Console.WriteLine("    " + s);
+                }
+            }
+        }
+
+        public static void printBoolArray(bool[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                bool s = arr[i];
+                Console.WriteLine(i.ToString() + ": " + s.ToString());
+            }
+        }
+
+        public static void printObjectArray(object[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                object s = arr[i];
+                Console.WriteLine(i.ToString() + ": " + s.ToString());
+            }
+        }
+        #endregion
+
+        #region parsers
+        public static List<string[]> txtPinginfoviewParser(string textFile, bool hasHeader)
+        {
+            List<string[]> result = new List<string[]>();
+
+            string[] lines = File.ReadAllLines(textFile);
+
+            int startLine = hasHeader ? 1 : 0;
+            for (int i = startLine; i < lines.Length; i++)
+            {
+                string s = lines[i].Trim();
+                int numberOfElements = 2;
+                string[] fields = s.Split(new string[] { " " }, numberOfElements, StringSplitOptions.None);
+
+                string[] elements = sanitizeStringArray(fields, numberOfElements);
+
+                if (elements[0].Length > 0)
+                    result.Add(elements);
+            }
+
+            return result;
+        }
+
+        public static List<string[]> csvHostListParser(string textFile, bool hasHeader)
+        {
+            List<string[]> result = new List<string[]>();
+
+            using (TextFieldParser parser = new TextFieldParser(textFile))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+
+                if (hasHeader) // skip the header if available
+                    parser.ReadLine();
+
+                while (!parser.EndOfData)
+                {
+                    string[] fields = parser.ReadFields();
+
+                    int numberOfElements = 2;
+
+                    string[] elements = sanitizeStringArray(fields, numberOfElements);
+
+                    if (elements[0].Length > 0)
+                        result.Add(elements);
+                }
+            }
+            return result;
+        }
+
+        #endregion
+
+        #region ping work
         public static string[] pingStatusTable = new string[] { "Online", "Timed out", "Unreachable", "TTL expired", "General failure" };
         private static readonly Dictionary<IPStatus, int> ipStatusToPingStatus = new Dictionary<IPStatus, int>()
         {
@@ -44,7 +179,7 @@ namespace Mother_of_Ping_CLI
         /// <param name="timeout">Timeout limit in ms</param>
         /// <param name="limitRate">Cap the rate down to 1 per second</param>
         /// <param name="replyTime">Output Roundtrip Time</param>
-        public static int ping(string ipAddr, int timeout, int bufferSize, int ttl, Boolean limitRate, out string replyAddr, out long replyTime, out int replyTtl)
+        public static int ping(string ipAddr, int timeout, int bufferSize, int ttl, out string replyAddr, out long replyTime, out int replyTtl, Boolean limitRate, int period)
         {
             int pingResult = ping(ipAddr, timeout, bufferSize, ttl, out replyAddr, out replyTime, out replyTtl);
 
@@ -56,16 +191,18 @@ namespace Mother_of_Ping_CLI
 
         public static int ping(string ipAddr, int timeout, int bufferSize, int ttl, out string replyAddr, out long replyTime, out int replyTtl)
         {
-            // Parse victim address. Failback to 0.0.0.0 if any error occurs (will result in "General failure").
+            // Parse victim address
             IPAddress IP;
-            string failbackIp = "0.0.0.0";
             try
             {
                 IP = IPAddress.Parse(ipAddr);
             }
             catch
             {
-                IP = IPAddress.Parse(failbackIp);
+                replyAddr = string.Empty;
+                replyTime = 0;
+                replyTtl = 0;
+                return 4;
             }
 
             // Create a new instant
@@ -85,11 +222,77 @@ namespace Mother_of_Ping_CLI
 
             // Now compiling result
             int pingResult = ipStatusToPingStatus[reply.Status];
-            replyAddr = reply.Address.ToString();
+            replyAddr = (reply.Address != null) ? reply.Address.ToString(): string.Empty; // it's null when timeout
             replyTime = (reply.Status == IPStatus.Success) ? reply.RoundtripTime : 0;
-            replyTtl = reply.Options.Ttl;
+            replyTtl = (reply.Options != null) ? reply.Options.Ttl : 0; // when unreachable, it's null
 
             return pingResult;
         }
+
+        public static void backgroundPing(string hostname, int period, int timeout, int bufferSize, int ttl, ref bool threadAlive, ref bool flushLogSignal, ref bool continueMonitor, ref string actualHost)
+        {
+            actualHost = hostname;
+
+            int totalCount = 0;
+            int downCount = 0;
+            int upCount = 0;
+            int consecutiveDownCount = 0;
+
+            while (continueMonitor)
+            {
+                threadAlive = true;
+
+                // measure how much time ping and other works take
+                Stopwatch watch = Stopwatch.StartNew();
+
+                int result = ping(hostname, timeout, bufferSize, ttl, out string replyAddr, out long replyTime, out int replyTtl);
+                // Console.WriteLine(tools.pingStatusTable[result] + " " + replyAddr + " " + replyTime.ToString() + " " + replyTtl.ToString());
+                totalCount++;
+                if (result == 0)
+                {
+                    upCount++;
+                    consecutiveDownCount = 0;
+                }
+                else
+                {
+                    downCount++;
+                    consecutiveDownCount++;
+                }
+
+                // todo: create log line
+
+                if (flushLogSignal)
+                {
+                    flushLogSignal = false;
+                    // todo: flush log
+                }
+
+                // then sleep for <period> - <time taken>
+                watch.Stop();
+                int elapsedMs = (int) watch.ElapsedMilliseconds;
+
+                //if (elapsedMs > timeout)
+                //{
+                //    Console.WriteLine(hostname + " missed: " + elapsedMs.ToString());
+                //}
+
+                int sleepTime = period - elapsedMs;
+                if (sleepTime > 0)
+                {
+                    Thread.Sleep(sleepTime);
+                }
+                //else
+                //{
+                //    Console.WriteLine(hostname + " will NOT sleep: " + sleepTime.ToString());
+                //}
+            }
+
+            // todo: flush log
+
+            //Console.WriteLine(hostname + ": exiting...");
+            Console.WriteLine(hostname + ": " + totalCount.ToString() + " total, " + upCount.ToString() + " up, " + downCount.ToString() + " down");
+
+        }
+        #endregion
     }
 }
